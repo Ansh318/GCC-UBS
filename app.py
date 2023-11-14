88a482263896dede7594fabe99263b7ed7cf42e7
Liam Lindy <llindy@vallis.io>
2023-11-14 13:21:18 -0500

    adding in Flask

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route('/')
 def index():
     if 'username' in session:
+        print(app.name)
         return f'Logged in as {session["username"]}'
     return 'You are not logged in'
 
