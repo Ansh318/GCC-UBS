15a22b2d1bc1b227ea11311346c28ebb3cd03233
Liam Lindy <llindy@vallis.io>
2023-11-14 17:00:04 -0500

    adding in Flask

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# GCC-UBS
-Develop and Deploy a Web Application on Cloud using Python and Flask.
+# GCC-UBS Mavericks
+> Develop and Deploy a Web Application on Cloud using Python and Flask.
+
+# Heroku 
+Heroku is a web deployment service that basically allows you to develop web-apps 
+in a few varieties; for our purposes, we are using Flask - a Python development framework.
+
+Heroku is a little hard to wrap your mind around because it ~just works~.
+
+Essentially how it is set up is that when a 
--- app.py
+++ app.py
@@ -1,17 +1,83 @@
 from flask import Flask, make_response, request, jsonify
 
-
 # Create Flask's `app` object
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
+    headers = {"Content-Type": "application/json"}
     if request.method == 'GET':
         my_dict = {'key': 'dictionary value'}
-        headers = {"Content-Type": "application/json"}
         return make_response(jsonify(my_dict), 200, headers)
     if request.method == 'POST':
         my_dict = {'key': request.args.get('language')}
         headers = {"Content-Type": "application/json"}
         return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.post("/file-reorganization")
+def file_reorganization():
+    # Set Up
+    headers = {"Content-Type": "application/json"}
+    my_dict = {}
+
+    # Do Work
+
+    # Return Value
+    return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.post("/portfolio-operations")
+def portfolio_operations():
+    headers = {"Content-Type": "application/json"}
+    my_dict = {}
+
+    # Do Work
+
+    # Return Value
+    return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.post("/time-intervals")
+def time_intervals():
+    headers = {"Content-Type": "application/json"}
+    my_dict = {}
+
+    # Do Work
+
+    # Return Value
+    return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.post("/data-encryption")
+def data_encryption():
+    headers = {"Content-Type": "application/json"}
+    my_dict = {}
+
+    # Do Work
+
+    # Return Value
+    return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.post("/coin-change")
+def coin_change():
+    headers = {"Content-Type": "application/json"}
+    my_dict = {}
+
+    # Do Work
+
+    # Return Value
+    return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.post("/risk-mitigation")
+def risk_mitigation():
+    headers = {"Content-Type": "application/json"}
+    my_dict = {}
+
+    # Do Work
+
+    # Return Value
+    return make_response(jsonify(my_dict), 200, headers)
