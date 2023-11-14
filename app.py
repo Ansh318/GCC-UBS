8ce18a46d11ed446c8a8cdea386e475624e98fc7
Liam Lindy <llindy@vallis.io>
2023-11-14 16:21:16 -0500

    adding in Flask

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ from flask import Flask, make_response, request, jsonify
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET','POST'])
 def hello():
     if request.method != 'GET':
         return make_response('Malformed request', 400)
-    my_dict = {'key': 'dictionary value'}
-    headers = {"Content-Type": "application/json"}
-    return make_response(jsonify(my_dict), 200, headers)
+    if request.method == 'GET':
+        my_dict = {'key': 'dictionary value'}
+        headers = {"Content-Type": "application/json"}
+        return make_response(jsonify(my_dict), 200, headers)
+    if request.method == 'POST':
+        my_dict = {'key': request.data}
+        headers = {"Content-Type": "application/json"}
+        return make_response(jsonify(my_dict), 200, headers)
