d1a1659cfd64fdbfc0c29452321e764d1e4a27b7
Liam Lindy <llindy@vallis.io>
2023-11-14 16:22:40 -0500

    adding in Flask

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def hello():
-    if request.method != 'GET':
-        return make_response('Malformed request', 400)
     if request.method == 'GET':
         my_dict = {'key': 'dictionary value'}
         headers = {"Content-Type": "application/json"}
