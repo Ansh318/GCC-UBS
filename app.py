afe1532786c4699007451df17b0d945067ba25c9
Ansh318 <85252414+Ansh318@users.noreply.github.com>
2023-11-14 21:54:45 -0500

    Update app.py
    integrated backend logic calls by importing backend.py for file reorganization and portfolio operation endpoints

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, make_response, request, jsonify
+import backend
+import json
 
 # Create Flask's `app` object
 app = Flask(__name__)
@@ -16,30 +18,30 @@ def hello():
         return make_response(jsonify(my_dict), 200, headers)
 
 
-@app.post("/file-reorganization")
+@app.route("/file-reorganization", methods=['GET', 'POST'])
 def file_reorganization():
     # Set Up
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
-
-    # Do Work
-
+    # Access the data sent in the POST request
+    #data = request.json  Assuming the data is sent as JSON
+    with open('sample_data.json', 'r') as json_file:
+        data = json.load(json_file)
+    my_dict = backend.max_palindrome_length(data)
     # Return Value
     return make_response(jsonify(my_dict), 200, headers)
 
-
-@app.post("/portfolio-operations")
+@app.route("/portfolio-operations", methods=['GET', 'POST'])
 def portfolio_operations():
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
-
-    # Do Work
-
+    #Access the data sent in the POST request
+    #data = request.json  Assuming the data is sent as JSON
+    with open('sample_data_2.json', 'r') as json_file:
+        data = json.load(json_file)
+    my_dict = backend.calculate_max_profit(data)
     # Return Value
     return make_response(jsonify(my_dict), 200, headers)
 
-
-@app.post("/time-intervals")
+@app.route("/time-intervals",methods=['GET', 'POST'])
 def time_intervals():
     headers = {"Content-Type": "application/json"}
     my_dict = {}
@@ -50,7 +52,7 @@ def time_intervals():
     return make_response(jsonify(my_dict), 200, headers)
 
 
-@app.post("/data-encryption")
+@app.route("/data-encryption", methods=['GET', 'POST'])
 def data_encryption():
     headers = {"Content-Type": "application/json"}
     my_dict = {}
@@ -61,7 +63,7 @@ def data_encryption():
     return make_response(jsonify(my_dict), 200, headers)
 
 
-@app.post("/coin-change")
+@app.route("/coin-change", methods=['GET', 'POST'])
 def coin_change():
     headers = {"Content-Type": "application/json"}
     my_dict = {}
@@ -72,7 +74,7 @@ def coin_change():
     return make_response(jsonify(my_dict), 200, headers)
 
 
-@app.post("/risk-mitigation")
+@app.route("/risk-mitigation", methods=['GET', 'POST'])
 def risk_mitigation():
     headers = {"Content-Type": "application/json"}
     my_dict = {}
