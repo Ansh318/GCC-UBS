35fc9cc14cc993a43bf5d5178502b5af9a20d4e4
Liam Lindy <llindy@vallis.io>
2023-11-15 12:04:28 -0500

    creating README + scafolding to keep things seperate

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import json
+
 from flask import Flask, make_response, request, jsonify
 
-# Create Flask's `app` object
+from solutions.file_reorganization import file_reorganization_solution
+
 app = Flask(__name__)
 
 
@@ -12,7 +15,6 @@ def hello():
         return make_response(jsonify(my_dict), 200, headers)
     if request.method == 'POST':
         my_dict = {'key': request.args.get('language')}
-        headers = {"Content-Type": "application/json"}
         return make_response(jsonify(my_dict), 200, headers)
 
 
@@ -20,64 +22,79 @@ def hello():
 def file_reorganization():
     # Set Up
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
+    """
+    POST -> { "inputs" : ["coding", "its harder to read code than to write it"] }
+    request.args.get('inputs') yields value 
+    """
+    inputs = json.loads(request.args.get('inputs'))  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
-
+    # results = file_reorganization_solution(inputs)
+    my_dict = {"inputs": inputs}
     # Return Value
     return make_response(jsonify(my_dict), 200, headers)
 
 
 @app.post("/portfolio-operations")
 def portfolio_operations():
+    # Set Up
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
+    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
+    results = file_reorganization_solution(inputs)
 
     # Return Value
-    return make_response(jsonify(my_dict), 200, headers)
+    return make_response(jsonify(results), 200, headers)
 
 
 @app.post("/time-intervals")
 def time_intervals():
+    # Set Up
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
+    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
+    results = file_reorganization_solution(inputs)
 
     # Return Value
-    return make_response(jsonify(my_dict), 200, headers)
+    return make_response(jsonify(results), 200, headers)
 
 
 @app.post("/data-encryption")
 def data_encryption():
+    # Setup
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
+    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
+    results = file_reorganization_solution(inputs)
 
     # Return Value
-    return make_response(jsonify(my_dict), 200, headers)
+    return make_response(jsonify(results), 200, headers)
 
 
 @app.post("/coin-change")
 def coin_change():
+    # Set Up
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
+    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
+    results = file_reorganization_solution(inputs)
 
     # Return Value
-    return make_response(jsonify(my_dict), 200, headers)
+    return make_response(jsonify(results), 200, headers)
 
 
 @app.post("/risk-mitigation")
 def risk_mitigation():
+    # Set Up
     headers = {"Content-Type": "application/json"}
-    my_dict = {}
+    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
+    results = file_reorganization_solution(inputs)
 
     # Return Value
-    return make_response(jsonify(my_dict), 200, headers)
+    return make_response(jsonify(results), 200, headers)
