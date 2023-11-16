96029ab7e6ef3f1be7296941f412f06c0051e1ea
Ansh318 <85252414+Ansh318@users.noreply.github.com>
2023-11-16 11:45:22 -0500

    Update app.py
    added imports and endpoints

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import json
 
 from flask import Flask, make_response, request, jsonify
 
+from solutions.profit_maximization import profit_maximization_solution
 from solutions.file_reorganization import file_reorganization_solution
 from solutions.portolio_operations import portfolio_operations_solution
+from solutions.mlmm_Program import mlmm_program_solution
 from solutions.data_encryption import data_encryption_solution
+from solutions.time_intervals import time_intervals_solution
+from solutions.fraudulent_transactions import fraudulent_transactions_solution
 from solutions.coin_change import coin_change_solution
 from solutions.risk_mitigation import risk_mitigation_solution
-from solutions.time_intervals import time_intervals_solution
-from solutions.profit_maximization import profit_maximization_solution
+
 
 app = Flask(__name__)
 
@@ -51,19 +54,13 @@ def portfolio_operations():
     # Return Value
     return make_response(jsonify(results), 200, headers)
 
-
-@app.post("/time-intervals")
-def time_intervals():
+@app.post("/mlmm-program")
+def mlmm_program():
     # Set Up
     headers = {"Content-Type": "application/json"}
-    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
-
-    # Do Work
-    results = file_reorganization_solution(inputs)
-
-    # Return Value
+    inputs = json.loads(request.args.get('inputs'))  # do some cleanup / conversion to as expected from JSON
+    results = mlmm_program_solution(inputs)
     return make_response(jsonify(results), 200, headers)
-
 
 @app.post("/data-encryption")
 def data_encryption():
@@ -72,11 +69,35 @@ def data_encryption():
     inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
-    results = file_reorganization_solution(inputs)
+    results = data_encryption_solution(inputs)
 
     # Return Value
     return make_response(jsonify(results), 200, headers)
 
+
+@app.post("/time-intervals")
+def time_intervals():
+    # Set Up
+    headers = {"Content-Type": "application/json"}
+    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
+
+    # Do Work
+    results = time_intervals_solution(inputs)
+
+    # Return Value
+    return make_response(jsonify(results), 200, headers)
+
+@app.post("/fradulent-transactions")
+def fraudulent_transactions():
+    # Set Up
+    headers = {"Content-Type": "application/json"}
+    inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
+
+    # Do Work
+    results = fraudulent_transactions_solution(inputs)
+
+    # Return Value
+    return make_response(jsonify(results), 200, headers)
 
 @app.post("/coin-change")
 def coin_change():
@@ -96,7 +117,7 @@ def risk_mitigation():
     inputs = request.args.get('inputs')  # do some cleanup / conversion to as expected from JSON
 
     # Do Work
-    results = file_reorganization_solution(inputs)
+    results = risk_mitigation_solution(inputs)
 
     # Return Value
     return make_response(jsonify(results), 200, headers)
