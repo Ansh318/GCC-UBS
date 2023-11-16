e55ace8f787c3d855e9ca984a648bb4915bc30bd
Ansh318 <85252414+Ansh318@users.noreply.github.com>
2023-11-16 15:07:52 -0500

    Update data_encryption.py
    implementation for data encryption

--- solutions/data_encryption.py
+++ solutions/data_encryption.py
@@ -1,2 +1,31 @@
 def data_encryption_solution():
-    print("hello_world")
+    result = {'answer':[]}
+    for i in range(len(input_data)):
+        input_data[i] = input_data[i].replace(" ", "")
+        input_str = input_data[i]
+        encrypt_str = encrypted(input_str)
+        result['answer'].append(encrypt_str)
+    return result
+
+def encrypted(input_string):
+    matrix_rows = math.floor(math.sqrt(len(input_string)))
+    matrix_cols = math.ceil(math.sqrt(len(input_string)))
+     # Ensure rows * columns >= string_length
+    while matrix_rows * matrix_cols < len(input_string):
+        matrix_rows += 1
+    # Initialize the matrix
+    matrix = [[''] * matrix_cols for _ in range(matrix_rows)]
+    k = 0
+    for i in range(matrix_rows):
+        for j in range(matrix_cols):
+            if k < len(input_string):
+                matrix[i][j] = input_string[k]
+                k += 1
+    result = ''
+    for j in range(matrix_cols):
+        for i in range(matrix_rows):
+            if matrix[i][j] != ' ':
+                result += matrix[i][j]
+        result += ' '
+
+    return result.strip()
