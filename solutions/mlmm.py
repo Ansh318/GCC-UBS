1347b8feeaf97a08e68aa9b71a3e0ee179268227
Liam Lindy <llindy@vallis.io>
2023-11-17 11:36:47 -0500

    updated mllm to match

--- solutions/mlmm.py
+++ solutions/mlmm.py
@@ -1,4 +1,26 @@
 def mlmm_program_solution(inputs):
-    i = inputs
-    print("Hello World")
-    return i
+    result = {'answer': []}
+    for i in range(len(inputs)):
+        cutoff = int(inputs[i][0])
+        scores = inputs[i][2].split()
+        scores = [int(num) for num in scores]
+        num_scores = len(scores)
+        max_bow = count_bow(scores, num_scores, cutoff)
+        result['answer'].append(max_bow)
+    return result
+
+
+def count_bow(scores, num_scores, cutoff):
+    count = 0
+    for i in range(0, num_scores):
+        summation = 0
+        for j in range(i, num_scores):
+            # If sum is less than k
+            # then update sum and 
+            # increment count
+            if summation + scores[j] < cutoff:
+                summation = scores[j] + summation
+                count += 1
+            else:
+                break
+    return count
